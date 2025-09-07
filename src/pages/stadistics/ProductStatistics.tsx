7a43649976e8a26637893f43e7d246060e52a97d
Denis Peralta <denisperaltac@gmail.com>
2025-09-07 19:42:05 -0300

    index

--- src/pages/stadistics/ProductStatistics.tsx
+++ src/pages/stadistics/ProductStatistics.tsx
@@ -484,6 +484,9 @@ const ProductStatistics: React.FC = () => {
                 <thead className="bg-gray-50">
                   <tr>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      #
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Producto
                     </th>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -506,6 +509,9 @@ const ProductStatistics: React.FC = () => {
                       key={producto.productoId}
                       className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
                     >
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {index + 1}
+                      </td>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div>
                           <div className="text-sm font-medium text-gray-900">
